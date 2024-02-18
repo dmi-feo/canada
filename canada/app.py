22a301a53f124ba18cc67414f20ddb919f205b79
Dmitriy Fedorov <dmifedorov@nebius.com>
2024-02-18 15:18:36 +0100

    refact

--- canada/app.py
+++ canada/app.py
@@ -1,5 +1,6 @@
 import os
 import logging
+from typing import Callable
 
 from aiohttp import web
 
@@ -10,34 +11,36 @@ import canada.api.workbook.views
 import canada.api.entry.views
 from canada import constants
 
-from canada.yt_client import SimpleYtClient, YTCookieAuthContext, YTAuthContext, YTNoAuthContext
+from canada.yt_client import SimpleYtClient, YTCookieAuthContext, YTNoAuthContext
 from canada.settings import CanadaSettings, YTAuthMode
 
 
-def get_yt_auth_context(settings: CanadaSettings, request: web.Request) -> YTAuthContext:
-    match settings.YT_AUTH_MODE:
-        case YTAuthMode.disabled:
-            return YTNoAuthContext()
-        case YTAuthMode.cookie_from_env:
-            return YTCookieAuthContext(cypress_cookie=os.environ["YT_COOKIE"], csrf_token=os.environ["YT_CSRF_TOKEN"])
-        case YTAuthMode.pass_request_creds:
-            return YTCookieAuthContext(
-                cypress_cookie=request.cookies[constants.YT_COOKIE_TOKEN_NAME],
-                csrf_token=request.headers[constants.YT_HEADER_CSRF_NAME]
-            )
-        case _:
-            raise ValueError(f"Unknown YTAuthMode: {settings.YT_AUTH_MODE}")
-
-
-def get_yt_cli_factory(settings: CanadaSettings):
+def get_yt_cli_noauth_factory(settings: CanadaSettings):
     def yt_cli_factory(request: web.Request) -> SimpleYtClient:
-        auth_context = get_yt_auth_context(settings, request)
+        auth_context = YTNoAuthContext()
+        return SimpleYtClient(yt_host=settings.YT_HOST, auth_context=auth_context, ca_file=settings.CA_FILE)
 
-        return SimpleYtClient(
-            yt_host=settings.YT_HOST,
-            auth_context=auth_context,
-            ca_file=settings.CA_FILE,
+    return yt_cli_factory
+
+
+def get_yt_cli_env_cookie_auth_factory(settings: CanadaSettings):
+    def yt_cli_factory(request: web.Request) -> SimpleYtClient:
+        auth_context = YTCookieAuthContext(
+            cypress_cookie=os.environ["YT_COOKIE"],
+            csrf_token=os.environ["YT_CSRF_TOKEN"]
         )
+        return SimpleYtClient(yt_host=settings.YT_HOST, auth_context=auth_context, ca_file=settings.CA_FILE)
+
+    return yt_cli_factory
+
+
+def get_yt_cli_request_cookie_auth_factory(settings: CanadaSettings):
+    def yt_cli_factory(request: web.Request) -> SimpleYtClient:
+        auth_context = YTCookieAuthContext(
+            cypress_cookie=request.cookies[constants.YT_COOKIE_TOKEN_NAME],
+            csrf_token=request.headers[constants.YT_HEADER_CSRF_NAME]
+        )
+        return SimpleYtClient(yt_host=settings.YT_HOST, auth_context=auth_context, ca_file=settings.CA_FILE)
 
     return yt_cli_factory
 
@@ -45,10 +48,21 @@ def get_yt_cli_factory(settings: CanadaSettings):
 def create_app(settings: CanadaSettings) -> web.Application:
     logging.basicConfig(level=logging.DEBUG)
 
+    yt_cli_factory: Callable[[web.Request], SimpleYtClient]
+    match settings.YT_AUTH_MODE:
+        case YTAuthMode.disabled:
+            yt_cli_factory = get_yt_cli_noauth_factory(settings)
+        case YTAuthMode.cookie_from_env:
+            yt_cli_factory = get_yt_cli_env_cookie_auth_factory(settings)
+        case YTAuthMode.pass_request_creds:
+            yt_cli_factory = get_yt_cli_request_cookie_auth_factory(settings)
+        case _:
+            raise ValueError(f"Unknown YTAuthMode: {settings.YT_AUTH_MODE}")
+
     app_instance = web.Application(
         middlewares=[
             attach_services(
-                yt_cli_factory=get_yt_cli_factory(settings),
+                yt_cli_factory=yt_cli_factory,
                 root_collection_node_id=settings.ROOT_COLLECTION_NODE_ID,
             ),
         ]
